ff98bce23d347b8c60ba10ea7656c7cd194d6c09
Jabyou <jabber0713@gmail.com>
2024-09-26 20:29:32 -0400

    Classes and objects

---
+++ Sep26.py
@@ -0,0 +1,10 @@
+#hard Classes
+class coding:
+    thing = "python"
+    code = "is hard"
+    next_code = "next code is harder"
+#impossible to do Objects
+my_code = coding()
+print(my_code.thing)
+print(my_code.code)
+print(my_code.next_code)
